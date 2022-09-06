83f6b189b985b37c87293c2564a674d04d9c03f7
Gamdara <47179398+Gamdara@users.noreply.github.com>
2022-09-06 16:36:56 +0800

    Bottom Menu

--- app/src/main/java/com/kel4/tubes_4_bioskop/MainActivity.kt
+++ app/src/main/java/com/kel4/tubes_4_bioskop/MainActivity.kt
@@ -2,10 +2,45 @@ package com.kel4.tubes_4_bioskop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.kel4.tubes_4_bioskop.fragments.PlayingFragment
+import com.kel4.tubes_4_bioskop.fragments.TicketFragment
+import com.kel4.tubes_4_bioskop.fragments.UpcomingFragment
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nav : Menu
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val myBottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        myBottomNavigationView.setOnItemSelectedListener {item ->
+            when (item.itemId) {
+                R.id.page_1 -> {
+                    changeFragment(PlayingFragment())
+                }
+                R.id.page_2 -> {
+                    changeFragment(UpcomingFragment())
+                }
+                R.id.page_3 -> {
+                    changeFragment(TicketFragment())
+                }
+            }
+            false
+
+        }
     }
+
+
+    fun changeFragment(fragment: Fragment?) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fgc, fragment)
+                .commit()
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/kel4/tubes_4_bioskop/fragments/PlayingFragment.kt
@@ -0,0 +1,20 @@
+package com.kel4.tubes_4_bioskop.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kel4.tubes_4_bioskop.R
+
+class PlayingFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_playing, container, false)
+    }
+
+}
