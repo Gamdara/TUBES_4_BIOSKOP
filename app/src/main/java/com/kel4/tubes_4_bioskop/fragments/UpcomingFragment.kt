2a670fb7e10fa7c6a164bc7700479a8c28bd4fe0
VeaPee <vap.agung@gmail.com>
2022-09-07 12:44:51 +0700

    Update 3 Upcoming Movie

--- app/src/main/java/com/kel4/tubes_4_bioskop/fragments/UpcomingFragment.kt
+++ app/src/main/java/com/kel4/tubes_4_bioskop/fragments/UpcomingFragment.kt
@@ -24,7 +24,7 @@ class UpcomingFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_upcoming, container, false)
 
     }
-
+    //Test
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val layoutManager = LinearLayoutManager(context)
