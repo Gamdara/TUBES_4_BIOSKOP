9f8a985a9c76f75dabc51c01569e64e18ec38f14
VeaPee <vap.agung@gmail.com>
2022-09-20 13:11:56 +0700

    Update 3 Upcoming Movie

--- app/src/main/java/com/kel4/tubes_4_bioskop/entity/NowPlaying.kt
+++ app/src/main/java/com/kel4/tubes_4_bioskop/entity/NowPlaying.kt
@@ -6,9 +6,9 @@ class NowPlaying(var judul:String, var sinopsis: String, var director: String, v
     companion object{
         @JvmField
         var listOfNowPlaying = arrayOf(
-            NowPlaying("Ngeri-Ngeri Sedap", "sinopsis", "director", "writter", R.drawable.poster_nowplaying_1),
-            NowPlaying("Mencuri Raden Saleh", "sinopsis", "director", "writter", R.drawable.poster_nowplaying_2),
-            NowPlaying("Cinta Pertama, Kedua&Ketiga", "sinopsis", "director", "writter", R.drawable.poster_nowplaying_3)
+            NowPlaying("Ngeri-Ngeri Sedap", "sinopsis", "director", "writter", R.drawable.poster_ngeringerisedap),
+            NowPlaying("Mencuri Raden Saleh", "sinopsis", "director", "writter", R.drawable.poster_mencuriradensaleh),
+            NowPlaying("Cinta Pertama, Kedua&Ketiga", "sinopsis", "director", "writter", R.drawable.poster_cintapertamakeduaketiga)
         )
     }
 
