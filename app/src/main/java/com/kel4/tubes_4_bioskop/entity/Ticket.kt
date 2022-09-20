0e780296d2eb0e8299cec0c0ac8c34cadbafb7cd
just4fun147 <pauluswindito1@gmail.com>
2022-09-20 13:55:18 +0700

    view fixed, fragment movie detail

--- app/src/main/java/com/kel4/tubes_4_bioskop/entity/Ticket.kt
+++ app/src/main/java/com/kel4/tubes_4_bioskop/entity/Ticket.kt
@@ -1,11 +1,13 @@
 package com.kel4.tubes_4_bioskop.entity
 
+import com.kel4.tubes_4_bioskop.entity.MovieList.listOfNowPlaying
+
 class Ticket (var movie: Movie, var seat: String, var time: String) {
     companion object{
         @JvmField
         var listOfTicket= arrayOf(
-            Ticket(Movie.listOfNowPlaying[0],"2D","18:30"),
-            Ticket(Movie.listOfNowPlaying[0],"2D","18:30")
+            Ticket(MovieList.listOfNowPlaying[0],"2D","18:30"),
+            Ticket(MovieList.listOfNowPlaying[0],"2D","18:30")
         )
     }
 }
