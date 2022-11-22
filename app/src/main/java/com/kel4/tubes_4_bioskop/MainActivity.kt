1891d81d2a73b1ee0372aa05ba0372429e6aa039
VeaPee <vap.agung@gmail.com>
2022-11-22 15:31:37 +0700

    Library PermissionHelper

--- app/src/main/java/com/kel4/tubes_4_bioskop/MainActivity.kt
+++ app/src/main/java/com/kel4/tubes_4_bioskop/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.kel4.tubes_4_bioskop
 
+import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.widget.ImageView
 import androidx.appcompat.app.AlertDialog
@@ -13,12 +16,34 @@ import com.kel4.tubes_4_bioskop.fragments.PlayingFragment
 import com.kel4.tubes_4_bioskop.fragments.TicketFragment
 import com.kel4.tubes_4_bioskop.fragments.UpcomingFragment
 import com.kel4.tubes_4_bioskop.pages.ProfileActivity
+import com.master.permissionhelper.PermissionHelper
 
 class MainActivity : AppCompatActivity() {
     lateinit var nav : Menu
+    internal var permissionHelper: PermissionHelper? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        permissionHelper = PermissionHelper(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 100)
+        permissionHelper?.denied {
+            if (it) {
+                Log.d(TAG, "Permission denied by system")
+                permissionHelper?.openAppDetailsActivity()
+            } else {
+                Log.d(TAG, "Permission denied")
+            }
+        }
+
+        //Request all permission
+        permissionHelper?.requestAll {
+            Log.d(TAG, "All permission granted")
+        }
+
+        //Request individual permission
+        permissionHelper?.requestIndividual {
+            Log.d(TAG, "Individual Permission Granted")
+        }
         val myBottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         myBottomNavigationView.setOnItemSelectedListener {item ->
             when (item.itemId) {
@@ -46,6 +71,11 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.fgc, fragment)
                 .commit()
         }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        permissionHelper?.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
 
